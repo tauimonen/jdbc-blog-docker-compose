6ab1baba881c2525262a67f104846a0f0545ec84
vgtoui <toni.uimonen@tuni.fi>
2023-12-07 17:56:04 +0200

    Create a MVP blog app

---
+++ README.md
@@ -0,0 +1,12 @@
+# Spring Data JDBC Blog App
+- Maven
+- Spring Boot 3.2
+- Java 21
+- Dependencies
+  - Web
+  - JDBC
+  - PostgreSQL
+  - Docker Compose
+
+schema.sql in resources to create a table and insert a row.
+In order to start the database container, your local Docker should be running
--- src/main/java/dev/tau/jdbcblog/JdbcBlogApplication.java
+++ src/main/java/dev/tau/jdbcblog/JdbcBlogApplication.java
@@ -1,7 +1,15 @@
 package dev.tau.jdbcblog;
 
+import dev.tau.jdbcblog.post.Post;
+import dev.tau.jdbcblog.post.PostRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class JdbcBlogApplication {
@@ -10,4 +18,16 @@ public class JdbcBlogApplication {
 		SpringApplication.run(JdbcBlogApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(PostRepository repository) {
+		return args -> {
+			repository.save(new Post("101","Hölön Pölön","hölön-pölön", LocalDate.now(),10,"Spring Boot",null));
+			List<Post> posts = repository.findAll();
+			System.out.println(posts);
+
+
+			Optional<Post> hello = repository.findBySlug("hölön-pölön");
+			System.out.println(hello);
+		};
+	}
 }
