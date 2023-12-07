79ac32824c0c59c38aeb72d85ab126945878034d
vgtoui <toni.uimonen@tuni.fi>
2023-12-07 13:47:28 +0200

    Create maven spring boot project

---
+++ src/main/java/dev/tau/jdbcblog/JdbcBlogApplication.java
@@ -0,0 +1,13 @@
+package dev.tau.jdbcblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tau/jdbcblog/JdbcBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tau.jdbcblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
